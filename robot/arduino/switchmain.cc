75ffcda01c62cff92453445ae249f1a7da4e1ba6
Kevin Kellar <kellar.kevin@gmail.com>
2020-03-21 09:50:33 -0700

    Can Build switchmain which is just a blink

--- robot/arduino/switchmain.cc
+++ robot/arduino/switchmain.cc
@@ -1,13 +1,18 @@
 
 #include "switch.h"
+#include <Arduino.h>
 
 void setup()
 {
-switch_init();
+  pinMode(13, OUTPUT);
 }
 
 void loop()
 {
 switch_test_all();
+digitalWrite(13, LOW);
+delay(1000);
+digitalWrite(13, HIGH);
+delay(1000);
 }
 
